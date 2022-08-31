c1f979da2dd5e7df907c91b64d29a1984470d231
Jhoseph Araujo <j.garibald@gmail.com>
2022-08-30 21:23:27 -0300

    fix bug redimensionar imagem

--- app/Http/Controllers/UploadImageController.php
+++ app/Http/Controllers/UploadImageController.php
@@ -8,7 +8,7 @@ class UploadImageController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $name = "IMG_" . time() . $request->upload->getClientOriginalExtension();
+        $name = "IMG_" . time() . '.' . $request->upload->getClientOriginalExtension();
         $request->upload->storeAs('images', $name);
 
         return response()->json(['fileName' => $name, 'uploaded' => true, 'url' => url("storage/images/$name")]);
--- resources/js/Pages/Editor.vue
+++ resources/js/Pages/Editor.vue
@@ -1,24 +1,24 @@
 <script setup>
-import AppLayout from '@/Layouts/AppLayout.vue';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import { useForm } from '@inertiajs/inertia-vue3';
-import Button from '../Components/Button.vue';
+import AppLayout from "@/Layouts/AppLayout.vue";
+import Editor from "ckeditor5-custom-build";
+import { useForm } from "@inertiajs/inertia-vue3";
+import Button from "../Components/Button.vue";
 
+const editor = Editor;
 
-
-const editor = ClassicEditor;
-var csrf_token = document.head.querySelector('meta[name="csrf-token"]').getAttribute('content');
+var csrf_token = document.head
+    .querySelector('meta[name="csrf-token"]')
+    .getAttribute("content");
 
 const editorConfig = {
     ckfinder: {
-        uploadUrl: route('upload-image.store', { _token: csrf_token })
-    }
+        uploadUrl: route("upload-image.store", { _token: csrf_token }),
+    },
 };
 
 const form = useForm({
-    editorData: ''
+    editorData: "",
 });
-
 </script>
 
 <template>
@@ -31,12 +31,15 @@ const form = useForm({
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-sm p-2">
-                    <pre>
-                        {{ csrf_token.text }}
-                    </pre>
+                <div
+                    class="bg-white overflow-hidden shadow-xl sm:rounded-sm p-2"
+                >
                     <form @submit.prevent="form.post(route('editor.store'))">
-                        <ckeditor :editor="editor" v-model="form.editorData" :config="editorConfig">
+                        <ckeditor
+                            :editor="editor"
+                            v-model="form.editorData"
+                            :config="editorConfig"
+                        >
                         </ckeditor>
                         <div class="mt-4">
                             <Button :disabled="form.processing">Salvar</Button>
@@ -47,3 +50,9 @@ const form = useForm({
         </div>
     </AppLayout>
 </template>
+
+<style>
+.ck-editor__editable_inline {
+    min-height: 300px;
+}
+</style>
